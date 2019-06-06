ad36bfc639aac67420685716b2e72fe468eb7bd6
papaurke <urosmiric88@gmail.com>
2019-06-06 13:01:53 +0200

    Resolved connecting to database twice problem

--- app/db/connection.js
+++ app/db/connection.js
@@ -15,13 +15,20 @@ class Connection {
         // setting the mongodb to useCreateIndex instead of ensure to avoid deprecation message
         mongoose.set('useCreateIndex', true);
 
-        // connection to db
-        const connectionString = `mongodb://${this.config.get('db.host')}:${this.config.get('db.port')}/${this.config.get('db.name')}`;
-        mongoose.connect(connectionString, { useNewUrlParser: true });
-        
+        // checking if already connected to MongoDB
+        if(mongoose.connection.readyState === 0) {
+            // connection to db
+            const connectionString = `mongodb://${this.config.get('db.host')}:${this.config.get('db.port')}/${this.config.get('db.name')}`;
+
+            mongoose.connect(connectionString, { useNewUrlParser: true })
+            .then(() => console.log('Connected to MongoDB.') )
+            .catch(err => console.log('Error connecting the MongoDB', err));
+        }
+
         return mongoose;
+
     }
 
 }
 
-module.exports = new Connection();
+module.exports = Connection;
--- app/models/Model.js
+++ app/models/Model.js
@@ -1,19 +1,14 @@
+const Connection = require('../db/connection');
+
 /**
  * Model class contains everything that is needed for all models
  * Including all models goes from here too, and anything that
  * is needed can be added here for other models to use it.
  */
-class Model {
+class Model extends Connection {
     constructor() {
-        const db = require('../db/connection');
-
-        this.mongoose = db.startMongo();
-
-        /**
-         * Including models globally
-         */
-        this.User    = require('./User.js');
-        this.Country = require('./Country.js');
+        super();
+        this.mongoose = this.startMongo();
 
         /**
          * Bcrypt - For hashing a password
